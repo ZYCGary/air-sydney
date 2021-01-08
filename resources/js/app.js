1fd17e5562264c419b93f8c7995fcfee71df66a6
Yuchen Zhang <zyc.gary@gmail.com>
2021-01-09 01:21:36 +1100

    Revert "Merge pull request #1 from ZYCGary/feature-vue3"
    This reverts commit 88289093a238b621ecc7eafc98c365e27bd8fda1, reversing
    changes made to 30c75cdd936db5f566cdc5012c210d4a3565c3a0.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,1 @@
-import {createApp, h} from 'vue'
-import {App, plugin} from '@inertiajs/inertia-vue3'
-import {InertiaProgress} from '@inertiajs/progress'
-
-const el = document.getElementById('app')
-
-InertiaProgress.init()
-
-createApp({
-    render: () => h(App, {
-        initialPage: JSON.parse(el.dataset.page),
-        resolveComponent: name => require(`./Pages/${name}`).default,
-    })
-}).use(plugin).mount(el)
+require('./bootstrap');
